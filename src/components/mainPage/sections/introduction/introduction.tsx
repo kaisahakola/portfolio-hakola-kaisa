33dd58184d540ebfc643763d24da8730585934f4
Kaisa Hakola <kaisahakola00@gmail.com>
2026-01-21 17:43:48 +0200

    Add things to introduction section

--- src/components/mainPage/sections/introduction/introduction.tsx
+++ src/components/mainPage/sections/introduction/introduction.tsx
@@ -1,9 +1,13 @@
 import "../styles.scss";
+import "./Introduction.scss";
+import { AboutMe } from "../../../../common/const/AboutMe";
 
 const Introduction = () => {
   return (
     <div className="sectionContainer">
       <h1>Introduction</h1>
+
+      <p id="introduction-description">{AboutMe.description}</p>
     </div>
   )
 }

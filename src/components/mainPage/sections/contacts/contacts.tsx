2e0125ba8c9fcd74f3e7db1ea4f60791e6ce2679
Kaisa Hakola <kaisahakola00@gmail.com>
2026-01-21 17:21:38 +0200

    Add things to contacts section

--- src/components/mainPage/sections/contacts/contacts.tsx
+++ src/components/mainPage/sections/contacts/contacts.tsx
@@ -1,9 +1,18 @@
 import "../styles.scss";
+import "./Contacs.scss";
+import LinkOrTextWithIcon from "../../../shared/LinkOrTextWithIcon/LinkOrTextWithIcon";
+import { ContactInfo } from "../../../../common/const/ContactInfo";
 
 const Contacts = () => {
   return (
     <div className="sectionContainer">
       <h1>Contacts</h1>
+
+      <div className="contact-info-container">
+        <LinkOrTextWithIcon iconType="email" text="kaisahakola00@gmail.com" />
+        <LinkOrTextWithIcon iconType="github" isExternal={true} text="kaisahakola" url={`https://${ContactInfo.github}`} />
+        <LinkOrTextWithIcon iconType="linkedin" isExternal={true} text="khakola" url={`https://${ContactInfo.linkedIn}`} />
+      </div>
     </div>
   )
 }

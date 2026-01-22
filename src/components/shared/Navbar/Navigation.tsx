a9dcd12281f78afe0c30ba91442694ad575162d9
Kaisa Hakola <kaisahakola00@gmail.com>
2026-01-22 18:17:15 +0200

    Remove unused imports

--- src/components/shared/Navbar/Navigation.tsx
+++ src/components/shared/Navbar/Navigation.tsx
@@ -1,4 +1,3 @@
-import { Link, NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
